19f0b19f104ae82e167645508dc48e4e3fbdc96f
ArtemSh13 <mozg1303@ya.ru>
2024-11-28 23:56:57 +0300

    Add toast showing to button click on main screen

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(
+                    this@MainActivity, "Нажали на кнопку \"Поиск\"",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        val libraryButtonClickListener: View.OnClickListener = View.OnClickListener {
+            Toast.makeText(
+                this@MainActivity, "Нажали на кнопку \"Медиатека\"",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        libraryButton.setOnClickListener(libraryButtonClickListener)
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        val settingsButtonClickListener: View.OnClickListener = View.OnClickListener {
+            Toast.makeText(
+                this@MainActivity, "Нажали на кнопку \"Настройки\"",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        settingsButton.setOnClickListener(settingsButtonClickListener)
     }
 }
