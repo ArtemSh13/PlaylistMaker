35c44c68920d9cf3e5b723daa72a819d5ad32eaf
ArtemSh13 <mozg1303@ya.ru>
2024-11-29 10:38:58 +0300

    Add back button to toolbar on Settings screen

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.example.playlistmaker
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +12,9 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val settingsToolbar: Toolbar = findViewById(R.id.settings_toolbar)
+        settingsToolbar.setNavigationIcon(R.drawable.baseline_arrow_back_24)
+        settingsToolbar.setNavigationOnClickListener { finish() }
     }
 }
