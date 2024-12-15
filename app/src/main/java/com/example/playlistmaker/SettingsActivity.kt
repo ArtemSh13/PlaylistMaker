4a85e577a117106a12fb1211be3f6b14623763cf
ArtemSh13 <mozg1303@ya.ru>
2024-12-15 19:56:49 +0300

    Rename file of arrow back icon

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +12,7 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val settingsToolbar: Toolbar = findViewById(R.id.settings_toolbar)
-        settingsToolbar.setNavigationIcon(R.drawable.baseline_arrow_back_24)
+        settingsToolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back)
         settingsToolbar.setNavigationOnClickListener { finish() }
     }
 }
