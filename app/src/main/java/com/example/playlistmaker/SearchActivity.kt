00f3a93a33e0e2cc0d91670396b67e894f646f31
ArtemSh13 <mozg1303@ya.ru>
2024-12-15 21:45:40 +0300

    Add mock TextWatcher to search bar

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.playlistmaker.databinding.ActivitySearchBinding
@@ -18,6 +21,20 @@ class SearchActivity : AppCompatActivity() {
         searchToolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back)
         searchToolbar.setNavigationOnClickListener { finish() }
 
+        val searchBar: EditText = binding.searchBar
+        val searchBarTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //TODO("Not yet implemented")
+            }
 
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                //TODO("Not yet implemented")
+            }
+        }
+        searchBar.addTextChangedListener(searchBarTextWatcher)
     }
 }
