02bc6b47d90cac72471b240936b1f9a9d8470366
ArtemSh13 <mozg1303@ya.ru>
2025-01-04 00:18:43 +0300

    Replace R.layout with binding for Settings screen

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.example.playlistmaker.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val settingsToolbar: Toolbar = findViewById(R.id.settings_toolbar)
+        val settingsToolbar: Toolbar = binding.settingsToolbar
         settingsToolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back)
         settingsToolbar.setNavigationOnClickListener { finish() }
     }
