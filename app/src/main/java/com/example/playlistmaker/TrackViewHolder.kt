d6b8edc7af7a667c979e3fc6e2328ad3cbd7f047
ArtemSh13 <mozg1303@ya.ru>
2025-02-10 21:18:49 +0300

    Make prototype of track item

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -8,12 +9,12 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackName: TextView = itemView.findViewById(R.id.track_name)
     private val artistName: TextView = itemView.findViewById(R.id.artist_name)
     private val trackTime: TextView = itemView.findViewById(R.id.track_time)
-    private val artworkUrl100: TextView = itemView.findViewById(R.id.artwork_url_100)
+    private val artworkUrl100: ImageView = itemView.findViewById(R.id.artwork_url_100)
 
     fun bind(model: Track) {
         this.trackName.text = model.trackName
         this.artistName.text = model.artistName
         this.trackTime.text = model.trackTime
-        this.artworkUrl100.text = model.artworkUrl100
+        this.artworkUrl100.tooltipText = model.artworkUrl100
     }
 }
