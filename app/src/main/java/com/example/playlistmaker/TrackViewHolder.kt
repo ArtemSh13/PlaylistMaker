937dcce5673d0f8c15075b99470aea020df8c074
ArtemSh13 <mozg1303@ya.ru>
2025-02-12 01:36:04 +0300

    Add downloading of track artworks from corresponding URLs

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -4,17 +4,18 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackName: TextView = itemView.findViewById(R.id.track_name)
     private val artistName: TextView = itemView.findViewById(R.id.artist_name)
     private val trackTime: TextView = itemView.findViewById(R.id.track_time)
-    private val artworkUrl100: ImageView = itemView.findViewById(R.id.artwork_url_100)
+    private val trackArtwork: ImageView = itemView.findViewById(R.id.track_artwork)
 
     fun bind(model: Track) {
         this.trackName.text = model.trackName
         this.artistName.text = model.artistName
         this.trackTime.text = model.trackTime
-        this.artworkUrl100.tooltipText = model.artworkUrl100
+        Glide.with(this.itemView).load(model.artworkUrl100).into(trackArtwork)
     }
 }
