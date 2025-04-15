f33f866993aebb1f24463c3e8882f033288dbe55
ArtemSh13 <mozg1303@ya.ru>
2025-04-15 19:40:20 +0300

    Add text resources for audio player screen

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -26,6 +26,11 @@ class AudioPlayerActivity : AppCompatActivity() {
 
         val track = intent.getSerializableExtra("clickedTrack") as Track
 
+        // Toolbar
+        binding.audioplayerToolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back)
+        binding.audioplayerToolbar.setNavigationOnClickListener { finish() }
+
+        // Artwork
         Glide.with(this)
             .load(track.getCoverArtwork())
             .placeholder(R.drawable.ic_artwork_placeholder_big)
@@ -33,12 +38,26 @@ class AudioPlayerActivity : AppCompatActivity() {
             .transform(RoundedCorners(4))
             .into(binding.trackArtwork)
 
+        // Main Info
         binding.trackName.text = track.trackName
         binding.artistName.text = track.artistName
+
+        // Track Description
+
+        binding.trackTime.trackDescKey.setText(R.string.TrackTime_AudioPlayerScreen_PlaylistMaker)
         binding.trackTime.trackDescValue.text = track.trackTime
+
+        binding.collectionName.trackDescKey.setText(R.string.CollectionName_AudioPlayerScreen_PlaylistMaker)
         binding.collectionName.trackDescValue.text = track.collectionName
+
+        binding.releaseDate.trackDescKey.setText(R.string.ReleaseDate_AudioPlayerScreen_PlaylistMaker)
         binding.releaseDate.trackDescValue.text = track.releaseDate.subSequence(0, 4)
+
+        binding.primaryGenreName.trackDescKey.setText(R.string.PrimaryGenreName_AudioPlayerScreen_PlaylistMaker)
         binding.primaryGenreName.trackDescValue.text = track.primaryGenreName
+
+        binding.country.trackDescKey.setText(R.string.Country_AudioPlayerScreen_PlaylistMaker)
         binding.country.trackDescValue.text = track.country
+
     }
 }
