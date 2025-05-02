647e2eef1adcadf42cf007bb69add8f3f473cb59
ArtemSh13 <mozg1303@ya.ru>
2025-05-02 23:25:16 +0300

    Code style. Replace binding with this.binding

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -79,20 +79,20 @@ class AudioPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityAudioplayerBinding.inflate(layoutInflater)
+        this.binding = ActivityAudioplayerBinding.inflate(layoutInflater)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
             val statusBar = insets.getInsets(WindowInsetsCompat.Type.statusBars())
             view.updatePadding(top = statusBar.top)
             insets
         }
-        setContentView(binding.root)
+        setContentView(this.binding.root)
 
         // Get the track from the previous activity
         val track = intent.getSerializableExtra("clickedTrack") as Track
 
         // Toolbar
-        binding.audioplayerToolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back)
-        binding.audioplayerToolbar.setNavigationOnClickListener { finish() }
+        this.binding.audioplayerToolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back)
+        this.binding.audioplayerToolbar.setNavigationOnClickListener { finish() }
 
         // Artwork
         Glide.with(this)
@@ -100,27 +100,27 @@ class AudioPlayerActivity : AppCompatActivity() {
             .placeholder(R.drawable.ic_artwork_placeholder_big)
             .error(R.drawable.ic_artwork_placeholder_big)
             .transform(RoundedCorners(4))
-            .into(binding.trackArtwork)
+            .into(this.binding.trackArtwork)
 
         // Main Info
-        binding.trackName.text = track.trackName
-        binding.artistName.text = track.artistName
+        this.binding.trackName.text = track.trackName
+        this.binding.artistName.text = track.artistName
 
         // Track Description
-        binding.trackTime.trackDescKey.setText(R.string.TrackTime_AudioPlayerScreen_PlaylistMaker)
-        binding.trackTime.trackDescValue.text = track.trackTime
+        this.binding.trackTime.trackDescKey.setText(R.string.TrackTime_AudioPlayerScreen_PlaylistMaker)
+        this.binding.trackTime.trackDescValue.text = track.trackTime
 
-        binding.collectionName.trackDescKey.setText(R.string.CollectionName_AudioPlayerScreen_PlaylistMaker)
-        binding.collectionName.trackDescValue.text = track.collectionName
+        this.binding.collectionName.trackDescKey.setText(R.string.CollectionName_AudioPlayerScreen_PlaylistMaker)
+        this.binding.collectionName.trackDescValue.text = track.collectionName
 
-        binding.releaseDate.trackDescKey.setText(R.string.ReleaseDate_AudioPlayerScreen_PlaylistMaker)
-        binding.releaseDate.trackDescValue.text = track.releaseDate
+        this.binding.releaseDate.trackDescKey.setText(R.string.ReleaseDate_AudioPlayerScreen_PlaylistMaker)
+        this.binding.releaseDate.trackDescValue.text = track.releaseDate
 
-        binding.primaryGenreName.trackDescKey.setText(R.string.PrimaryGenreName_AudioPlayerScreen_PlaylistMaker)
-        binding.primaryGenreName.trackDescValue.text = track.primaryGenreName
+        this.binding.primaryGenreName.trackDescKey.setText(R.string.PrimaryGenreName_AudioPlayerScreen_PlaylistMaker)
+        this.binding.primaryGenreName.trackDescValue.text = track.primaryGenreName
 
-        binding.country.trackDescKey.setText(R.string.Country_AudioPlayerScreen_PlaylistMaker)
-        binding.country.trackDescValue.text = track.country
+        this.binding.country.trackDescKey.setText(R.string.Country_AudioPlayerScreen_PlaylistMaker)
+        this.binding.country.trackDescValue.text = track.country
 
         // MediaPlayer
         this.prepareAudioPlayer(track)
