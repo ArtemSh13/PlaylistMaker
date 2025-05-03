2f6fd87c7719fd7060c4216f1a0a2650bf252995
ArtemSh13 <mozg1303@ya.ru>
2025-05-03 07:34:59 +0300

    Add autosearch after 2 second since input end

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,6 +3,8 @@ package com.example.playlistmaker
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
@@ -24,11 +26,104 @@ import retrofit2.Response
 class SearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
 
-    private var searchBarInputTextValue = INPUT_TEXT_DEF
-
     companion object {
         const val SEARCH_BAR_INPUT_TEXT = "SEARCH_BAR_INPUT_TEXT"
         const val INPUT_TEXT_DEF = ""
+        private const val SEARCH_DEBOUNCE_DELAY = 2000L
+    }
+
+    private var searchBarInputTextValue = INPUT_TEXT_DEF
+
+    private val onTrackClickCallback = { track: Track ->
+        startActivity(
+            Intent(
+                this@SearchActivity,
+                AudioPlayerActivity::class.java
+            )
+                .putExtra("clickedTrack", track)
+        )
+    }
+
+    private fun showTrackList() {
+        this.binding.stub.visibility = View.GONE
+        this.binding.searchScreenStubUpdateButton.visibility = View.GONE
+
+        this.binding.trackList.visibility = View.VISIBLE
+    }
+
+    private fun clearTrackList() {
+        this.binding.trackList.adapter = TrackAdapter(tracks = emptyList(), onTrackClick = { })
+    }
+
+    private fun showNothingFoundStub() {
+        this.binding.trackList.visibility = View.GONE
+        this.binding.searchScreenStubUpdateButton.visibility = View.GONE
+
+        this.binding.stubPrimaryText.setText(R.string.search_screen_stub_nothing_found_primary_text)
+        this.binding.stubSecondaryText.setText(R.string.search_screen_stub_nothing_found_secondary_text)
+        this.binding.stubImage.setImageResource(R.drawable.img_nothing_found)
+
+        this.binding.stub.visibility = View.VISIBLE
+    }
+
+    private fun showConnectionProblemStub() {
+        this.binding.trackList.visibility = View.GONE
+
+        this.binding.stubPrimaryText.setText(R.string.search_screen_stub_connection_problem_primary_text)
+        this.binding.stubSecondaryText.setText(R.string.search_screen_stub_connection_problem_secondary_text)
+        this.binding.stubImage.setImageResource(R.drawable.img_connection_problem)
+        this.binding.searchScreenStubUpdateButton.visibility = View.VISIBLE
+
+        this.binding.stub.visibility = View.VISIBLE
+    }
+
+    private fun showTracksHistory() {
+        val tracksHistory = SharedPreferencesKeeper.getTracksHistory()
+        if (tracksHistory.isNotEmpty()) {
+            this.binding.trackList.visibility = View.GONE
+            this.binding.stub.visibility = View.GONE
+            this.binding.trackHistoryTrackList.adapter =
+                TrackAdapter(tracks = tracksHistory, onTrackClick = onTrackClickCallback)
+            this.binding.trackHistory.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideTracksHistory() {
+        this.clearTrackList()
+        this.binding.trackList.visibility = View.VISIBLE
+        this.binding.stub.visibility = View.GONE
+        this.binding.trackHistory.visibility = View.GONE
+    }
+
+    private val mainHandler = Handler(Looper.getMainLooper())
+    private val searchRunnable = Runnable {
+        iTunesAPIService.instance.getSongsByTerm(binding.searchBar.text.toString(), "music")
+            .enqueue(object : Callback<iTunesResponse> {
+                override fun onResponse(
+                    call: Call<iTunesResponse>,
+                    response: Response<iTunesResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        if (response.body()!!.resultCount > 0) {
+                            showTrackList()
+                            val responseTracks = response.body()?.results.orEmpty()
+                            binding.trackList.adapter = TrackAdapter(
+                                tracks = responseTracks,
+                                onTrackClick = onTrackClickCallback
+                            )
+                        } else {
+                            showNothingFoundStub()
+                        }
+                    } else {
+                        showConnectionProblemStub()
+                    }
+                }
+
+                override fun onFailure(call: Call<iTunesResponse>, t: Throwable) {
+                    t.printStackTrace()
+                    showConnectionProblemStub()
+                }
+            })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,64 +138,6 @@ class SearchActivity : AppCompatActivity() {
         setContentView(this.binding.root)
         SharedPreferencesKeeper.initSharedPreferencesFromContext(this)
 
-        val onTrackClickCallback = { track: Track ->
-            startActivity(
-                Intent(
-                    this@SearchActivity,
-                    AudioPlayerActivity::class.java
-                )
-                    .putExtra("clickedTrack", track)
-            )
-        }
-
-        fun showTrackList() {
-            this.binding.stub.visibility = View.GONE
-            this.binding.searchScreenStubUpdateButton.visibility = View.GONE
-
-            this.binding.trackList.visibility = View.VISIBLE
-        }
-
-        fun clearTrackList() { this.binding.trackList.adapter = TrackAdapter(tracks = emptyList(), onTrackClick = { }) }
-
-        fun showNothingFoundStub() {
-            this.binding.trackList.visibility = View.GONE
-            this.binding.searchScreenStubUpdateButton.visibility = View.GONE
-
-            this.binding.stubPrimaryText.setText(R.string.search_screen_stub_nothing_found_primary_text)
-            this.binding.stubSecondaryText.setText(R.string.search_screen_stub_nothing_found_secondary_text)
-            this.binding.stubImage.setImageResource(R.drawable.img_nothing_found)
-
-            this.binding.stub.visibility = View.VISIBLE
-        }
-
-        fun showConnectionProblemStub() {
-            this.binding.trackList.visibility = View.GONE
-
-            this.binding.stubPrimaryText.setText(R.string.search_screen_stub_connection_problem_primary_text)
-            this.binding.stubSecondaryText.setText(R.string.search_screen_stub_connection_problem_secondary_text)
-            this.binding.stubImage.setImageResource(R.drawable.img_connection_problem)
-            this.binding.searchScreenStubUpdateButton.visibility = View.VISIBLE
-
-            this.binding.stub.visibility = View.VISIBLE
-        }
-
-        fun showTracksHistory() {
-            val tracksHistory = SharedPreferencesKeeper.getTracksHistory()
-            if (tracksHistory.isNotEmpty()) {
-                this.binding.trackList.visibility = View.GONE
-                this.binding.stub.visibility = View.GONE
-                this.binding.trackHistoryTrackList.adapter = TrackAdapter(tracks = tracksHistory, onTrackClick = onTrackClickCallback)
-                this.binding.trackHistory.visibility = View.VISIBLE
-            }
-        }
-
-        fun hideTracksHistory() {
-            clearTrackList()
-            this.binding.trackList.visibility = View.VISIBLE
-            this.binding.stub.visibility = View.GONE
-            this.binding.trackHistory.visibility = View.GONE
-        }
-
         // Toolbar
         this.binding.searchToolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back)
         this.binding.searchToolbar.setNavigationOnClickListener { finish() }
@@ -110,8 +147,8 @@ class SearchActivity : AppCompatActivity() {
             this.binding.searchBar.text.clear()
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(this.binding.root.windowToken, 0)
-            clearTrackList()
-            showTrackList()
+            this.clearTrackList()
+            this.showTrackList()
         }
 
         val searchBarTextWatcher = object : TextWatcher {
@@ -125,6 +162,7 @@ class SearchActivity : AppCompatActivity() {
                     clearTrackList()
                     showTrackList()
                 }
+                mainHandler.removeCallbacks(searchRunnable)
             }
 
             override fun afterTextChanged(p0: Editable?) {
@@ -133,6 +171,7 @@ class SearchActivity : AppCompatActivity() {
                     showTracksHistory()
                 } else {
                     hideTracksHistory()
+                    mainHandler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
                 }
             }
         }
@@ -149,29 +188,8 @@ class SearchActivity : AppCompatActivity() {
         // Track list
         this.binding.trackList.layoutManager = LinearLayoutManager(this)
 
-        val callbackiTunesAPIService = object : Callback<iTunesResponse>{
-            override fun onResponse(call: Call<iTunesResponse>, response: Response<iTunesResponse>) {
-                if (response.isSuccessful) {
-                    if (response.body()!!.resultCount > 0) {
-                        showTrackList()
-                        val responseTracks = response.body()?.results.orEmpty()
-                        binding.trackList.adapter = TrackAdapter(tracks = responseTracks, onTrackClick = onTrackClickCallback)
-                    } else {
-                        showNothingFoundStub()
-                    }
-                } else {
-                    showConnectionProblemStub()
-                }
-            }
-
-            override fun onFailure(call: Call<iTunesResponse>, t: Throwable) {
-                t.printStackTrace()
-                showConnectionProblemStub()
-            }
-        }
-
-        this.binding.searchScreenStubUpdateButton.setOnClickListener { iTunesAPIService.instance.getSongsByTerm(binding.searchBar.text.toString(), "music")
-            .enqueue(callbackiTunesAPIService)
+        this.binding.searchScreenStubUpdateButton.setOnClickListener {
+            this.searchRunnable
         }
 
         this.binding.clearTracksHistoryButton.setOnClickListener {
@@ -181,11 +199,7 @@ class SearchActivity : AppCompatActivity() {
 
         this.binding.searchBar.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE && this.binding.searchBar.text.isNotEmpty()) {
-                iTunesAPIService.instance.getSongsByTerm(this.binding.searchBar.text.toString(), "music")
-                    .enqueue(
-                        callbackiTunesAPIService
-                    )
-                true
+                this.searchRunnable
             }
             false
         }
@@ -200,7 +214,7 @@ class SearchActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         val savedInstanceStateValue = savedInstanceState.getString(SEARCH_BAR_INPUT_TEXT)
         if (savedInstanceStateValue != null) {
-            searchBarInputTextValue = savedInstanceStateValue
+            this.searchBarInputTextValue = savedInstanceStateValue
         }
     }
 }
